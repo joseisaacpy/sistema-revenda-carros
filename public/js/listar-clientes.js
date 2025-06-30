bfc8df485d49c1aa68bfb76e1fe019c92f87b42e
José Isaac <joseisaacnascimento@gmail.com>
2025-06-29 21:14:35 -0300

    feat: Atualiza como a resposta da requisição virá - Atualmente, virá em formato de tabela

--- public/js/listar-clientes.js
+++ public/js/listar-clientes.js
@@ -1,25 +1,30 @@
-const clientes = document.getElementById("clientes-cadastrados");
 async function listarClientes() {
+  const clientes = document.getElementById("clientes-cadastrados");
   const response = await fetch("/api/clientes");
   const data = await response.json();
   data.forEach((cliente) => {
     const li = document.createElement("li");
+    let dataFormatada = new Date(cliente.dataNascimento).toLocaleDateString(
+      "pt-BR"
+    );
+
     li.innerHTML = `
           <div class="bg-white shadow-md p-4 rounded-md border border-gray-200">
             <p class="text-lg font-semibold">Nome: ${cliente.nome}</p>
             <p class="text-lg font-semibold">CPF: ${cliente.cpf}</p>
-            <p class="text-lg font-semibold">Data de Nascimento: ${cliente.dataNascimento}</p>
+            <p class="text-lg font-semibold">Data de Nascimento: ${dataFormatada}</p>
             <p class="text-lg font-semibold">E-mail: ${cliente.email}</p>
             <p class="text-lg font-semibold">Telefone: ${cliente.telefone}</p>
             <div class="mt-4 space-x-2">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</button>
-              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Excluir</button>
+              <button data-id="${cliente.id}" data-nome="${cliente.nome}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</button>
+              <button data-id="${cliente.id}" data-nome="${cliente.nome}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Excluir</button>
             </div>
           </div>
         `;
     clientes.appendChild(li);
   });
 }
+
 import { initMenuToggle } from "./menu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
--- public/js/listar-veiculos.js
+++ public/js/listar-veiculos.js
@@ -1,28 +1,24 @@
-const veiculos = document.getElementById("veiculos-cadastrados");
 async function listarVeiculos() {
+  const veiculos = document.getElementById("veiculos-cadastrados");
   const response = await fetch("/api/veiculos");
   const data = await response.json();
   data.forEach((veiculo) => {
     const li = document.createElement("li");
-    data.forEach((veiculo) => {
-      const li = document.createElement("li");
-      li.innerHTML = `
+    li.innerHTML = `
           <div class="bg-white shadow-md p-4 rounded-md border border-gray-200">
             <p class="text-lg font-semibold">Modelo: ${veiculo.modelo}</p>
             <p class="text-lg font-semibold">Marca: ${veiculo.marca}</p>
             <p class="text-lg font-semibold">Ano de Fabricação: ${veiculo.ano}</p>
             <p class="text-lg font-semibold">Preço (R$): ${veiculo.preco}</p>
             <div class="mt-4 space-x-2">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</button>
-              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Excluir</button>
+              <button data-id="${veiculo.id}" data-name="${veiculo.modelo}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</button>
+              <button data-id="${veiculo.id}" data-name="${veiculo.modelo}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Excluir</button>
             </div>
           </div>
         `;
-      veiculos.appendChild(li);
-    });
+    veiculos.appendChild(li);
   });
 }
-
 import { initMenuToggle } from "./menu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
